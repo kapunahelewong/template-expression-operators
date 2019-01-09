824ed9f9001bc81cdc26e3a8938f0bd041cd0c51
Kapunahele Wong <kapunahelewong@gmail.com>
2019-01-09 16:31:44 -0500

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,31 @@
 import { Component } from '@angular/core';
 
+// import { Hero } from './hero';
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  title = 'Template Expression Operators';
+  // currentHero: hero;
+
+  product = {
+    name: 'frimfram',
+    price: 42
+  };
+
+
+
+
+  hero = {
+    name : 'Bob',
+    birthdate : new Date(1930, 1, 25),
+    price: 98
+  };
+
+  nullhero = null;
+
 }
+
